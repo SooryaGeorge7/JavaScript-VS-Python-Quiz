0067807db45c2a4d949759bf227a383cb58f1d43
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-02-08 14:32:01 +0000

    add questions and answers in questions.js as objects in an array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,8 +1,163 @@
 const questions = [
     {
-        "question":
-        "choice":
+        "question": "console.log()",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":"JAVASCRIPT"
+    },
+    {
+        "question":"In which programming language is the word true a boolean value?",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":"string()",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":"Comments in code editor starts with #",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":"patient_name is an example variable name of:",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":"The datatype undefined represents a variable without a value in:",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":"=== means equal to (with comparison of data type) in:",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":"The logical operator OR has the syntax || in:",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":"The operator type() will return the type of data being operated on in:",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":"The variable item is defined as item='' in:",
+        "choice": ["PYTHON", "JAVASCRIPT"]
         "correct":
     }
-    
+    {
+        "question":"In which programming language do they not differentiate between integers and floats?",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":"An empty [] or {} is considered falsey in:",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    }
+    {
+        "question":" / colons  are more frequently used in :",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question": "The key $ is used to insert a variable into a string in:",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    }
+    {
+        "question":"if: elif: else: is the conditional logic syntax for",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":" {}/curly brackets are used to create objects in:",
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    }
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    }
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    }
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    }
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    }
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    },
+    {
+        "question":
+        "choice": ["PYTHON", "JAVASCRIPT"]
+        "correct":
+    }
 ]
+

6ac7721f25faa2f5a5c411b03c4fbe237c54cf41
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-02-08 14:02:16 +0000

    add function to start game in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ let gamePage = document.getElementById('game-page')
 let validationMessage = document.getElementById('validation')
 let scorePage = document.getElementById('scoreboard-area')
 
+
 //username validation 
 let usernameLength = "Please enter a name longer than 2 charectors";
 function validateMessage() {
@@ -15,4 +16,23 @@ function validateMessage() {
         playGame()
     }
 
+}
+ //game starrts with timer and first question
+
+let questionNumber =document.getElementById('question-number')
+let score = document.getElementById('your-score')
+let timer = document.getElementById('timer')
+let questionsLeft = document.getElementById('question-remaining')
+
+let userscore = 0;
+let qNumber = 1;
+let qLeft = 10;
+
+function playGame() {
+    gamePage.style.display = "block";
+    homePage.style.display = "none";
+    displayQuestions()
+    questionNumber.innerText = qNumber;
+    score.innerText = userscore;
+    questionsLeft.innerText = qLeft;
 }

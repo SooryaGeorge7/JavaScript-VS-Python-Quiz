0a63d8347d658fa686b25731cdd76d22bb8dc781
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-02-08 12:10:29 +0000

    add function in script.js to validate username

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 let playButton = document.getElementById('start-game')
 let userName = document.getElementById('username')
 let homePage = document.getElementById('home-page')
+let gamePage = document.getElementById('game-page')
+let validationMessage = document.getElementById('validation')
+let scorePage = document.getElementById('scoreboard-area')
+
+//username validation 
+let usernameLength = "Please enter a name longer than 2 charectors";
+function validateMessage() {
+    if(userName.value.length <= parseInt(2)) {
+        validationMessage.innerHTML = usernameLength;
+    }
+    else {
+        playGame()
+    }
+
+}

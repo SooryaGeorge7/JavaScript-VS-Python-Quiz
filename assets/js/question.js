e6b36d3d0c0e5f2c0e76c8141e1e9b0fc48b264d
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-02-08 16:10:08 +0000

    add displayQuestion function in script.js

---
+++ assets/js/question.js
@@ -0,0 +1,109 @@
+const questions = [
+    {
+        question: "console.log()",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct:"JAVASCRIPT"
+    },
+    {
+        question:"In which programming language is the word true a boolean value?",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct:"JAVASCRIPT"
+    },
+    {
+        question:"string()",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct:"PYTHON"
+    },
+    {
+        question:"Comments in code editor starts with #",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct: "PYTHON"
+    },
+    {
+        question:"patient_name is an example variable name of:",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct: "PYTHON"
+    },
+    {
+        question:"The datatype undefined represents a variable without a value in:",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct: "JAVASCRIPT"
+    },
+    {
+        question:"=== means equal to (with comparison of data type) in:",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct:"JAVASCRIPT"
+    },
+    {
+        question:"The logical operator OR has the syntax || in:",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct:"JAVASCRIPT"
+    },
+    {
+        question:"The operator type() will return the type of data being operated on in:",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct: "PYTHON"
+    },
+    {
+        question:"The variable item is defined as item='' in:",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct:"PYTHON"
+    },
+    {
+        question:"In which programming language do they not differentiate between integers and floats?",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct:"JAVASCRIPT"
+    },
+    {
+        question:"An empty [] or {} is considered falsey in:",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct:"PYTHON"
+    },
+    {
+        question:" :/colons  are more frequently used in :",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct:"PYTHON"
+    },
+    {
+        question: "The key $ is used to insert a variable into a string in:",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct:"JAVASCRIPT"
+    },
+    {
+        question:"if: elif: else: is the conditional logic syntax for",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct:"PYTHON"
+    },
+    {
+        question:" {}/curly brackets are used to create objects in:",
+        choice: ["PYTHON", "JAVASCRIPT"],
+        correct:"JAVASCRIPT"
+    },
+    {
+        question:
+        choice: ["PYTHON", "JAVASCRIPT"]
+        correct:
+    },
+    {
+        question:
+        choice: ["PYTHON", "JAVASCRIPT"]
+        correct:
+    },
+    {
+        question:
+        choice: ["PYTHON", "JAVASCRIPT"]
+        correct:
+    },
+    {
+        question:
+        choice: ["PYTHON", "JAVASCRIPT"]
+        correct:
+    },
+    {
+        question:
+        choice: ["PYTHON", "JAVASCRIPT"]
+        correct:
+    }
+    
+];
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
-let playButton = document.getElementById('start-game')
-let userName = document.getElementById('username')
-let homePage = document.getElementById('home-page')
-let gamePage = document.getElementById('game-page')
-let validationMessage = document.getElementById('validation')
-let scorePage = document.getElementById('scoreboard-area')
+let playButton = document.getElementById('start-game');
+let userName = document.getElementById('username');
+let homePage = document.getElementById('home-page');
+let gamePage = document.getElementById('game-page');
+let validationMessage = document.getElementById('validation');
+let scorePage = document.getElementById('scoreboard-area');
+let questionArea = document.getElementById('questions');
+let python = document.getElementById('python');
+let javaScript = document.getElementById('javascript');
+let questionNumber =document.getElementById('question-number');
+let score = document.getElementById('your-score');
+let timer = document.getElementById('timer');
+let questionsLeft = document.getElementById('question-remaining');
 
+let userscore = 0;
+let qNumber = 1;
+let qLeft = 10;
+let currentIndex = 0;
 
 //username validation 
 let usernameLength = "Please enter a name longer than 2 charectors";
@@ -13,26 +24,26 @@ function validateMessage() {
         validationMessage.innerHTML = usernameLength;
     }
     else {
-        playGame()
+        playGame();
     }
 
 }
  //game starrts with timer and first question
 
-let questionNumber =document.getElementById('question-number')
-let score = document.getElementById('your-score')
-let timer = document.getElementById('timer')
-let questionsLeft = document.getElementById('question-remaining')
 
-let userscore = 0;
-let qNumber = 1;
-let qLeft = 10;
-
+//play game
 function playGame() {
     gamePage.style.display = "block";
     homePage.style.display = "none";
-    displayQuestions()
+    displayQuestions(questions[currentIndex]);
     questionNumber.innerText = qNumber;
     score.innerText = userscore;
-    questionsLeft.innerText = qLeft;
+    questionsLeft.innerText= qLeft;
+}
+
+function displayQuestions(question) {
+    questionArea.innerText = question.question;
+    python.innerText = question.choice[0];
+    javaScript.innerText = question.choice[1];
+
 }

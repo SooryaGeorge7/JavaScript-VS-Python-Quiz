979e4246a0ddbfb39ca5d2b5a1f41e2232354f76
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-02-08 11:13:20 +0000

    add information on how to play game in home page, and create variables for home page in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,3 @@
-console.log("Connected!")
+let playButton = document.getElementById('start-game')
+let userName = document.getElementById('username')
+let homePage = document.getElementById('home-page')
